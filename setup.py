0a2b61ce7a0a4ca1b44be4c57420e21f58cba7b9
Troys-Code <41653822+Troys-Code@users.noreply.github.com>
2023-11-07 10:16:35 -0800

    Small issue

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from setuptools import setup, find_packages
 
 setup(
@@ -18,24 +17,3 @@ setup(
         ],
     },
 )
-=======
-from setuptools import setup, find_packages
-
-setup(
-    name='discord-ai-bot',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'torch',
-        'matplotlib',
-        'diffusers',
-        'transformers',
-        'discord.py',
-    ],
-    entry_points={
-        'console_scripts': [
-            'start-discord-bot=DiscordAI_Bot:start_discord_bot',
-        ],
-    },
-)
->>>>>>> Stashed changes
